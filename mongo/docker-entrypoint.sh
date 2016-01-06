39a1fcbf3997f3989087c9529573c13fd9301e1e
Maxime Cottret <mcottret@itrust.fr>
2016-01-06 14:44:05 +0100

    add custom mongo image : just overload docker-entrypoint.sh to allow running a mongojs script at startup

---
+++ mongo/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+set -x
+if [ "${1:0:1}" = '-' ]; then
+	set -- mongod "$@"
+fi
+
+if [ "$1" = 'mongod' ]; then
+	chown -R mongodb /data/db
+
+	numa='numactl --interleave=all'
+	if $numa true &> /dev/null; then
+		set -- $numa "$@"
+	fi
+
+    if [ "$MONGO_STARTUP_SCRIPT" != "" ]; then
+		gosu mongodb "$@" --fork --logpath /tmp/mongo.log --pidfilepath /tmp/mongo.pid
+        gosu mongodb mongo $MONGO_STARTUP_SCRIPT
+		gosu mongodb "$@" --shutdown
+		gosu mongodb rm /tmp/mongo.log
+    fi
+	exec gosu mongodb "$@"
+fi
+
+exec "$@"
