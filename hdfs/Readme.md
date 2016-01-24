c640b63ef6191ebd82fcc94af439aa875927add2
Maxime Cottret <mcottret@itrust.fr>
2016-01-24 23:00:43 +0100

    add hdfs image

---
+++ hdfs/Readme.md
@@ -0,0 +1,7 @@
+# Hadoop FS
+inspired by [Zoe](https://github.com/DistributedSystemsGroup/zoe-docker-images) images
+
+replica set to 1
+permissions are disable by default
+
+tested with provided docker-compose and docker 1.9
---
+++ hdfs/files/start-datanode.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd /opt/scripts
+
+cat core-site.xml | sed -e "s/XXX_NAMENODE_HOST/$NAMENODE_HOST/" > ${HADOOP_HOME}/etc/hadoop/core-site.xml
+cp hdfs-site.xml ${HADOOP_HOME}/etc/hadoop/
+
+${HADOOP_HOME}/bin/hdfs datanode
